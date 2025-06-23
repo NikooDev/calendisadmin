0a716caa7efc4826ac5f05a9fbe65051125d8350
nicolas <nicolas.tual@outlook.fr>
2025-06-23 03:14:37 +0200

    First commit

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTT-67ES94X4yInoqM344ghzs5K6N0FYU",
+  authDomain: "pompiers-tiffauges.firebaseapp.com",
+  projectId: "pompiers-tiffauges",
+  storageBucket: "pompiers-tiffauges.firebasestorage.app",
+  messagingSenderId: "730006798062",
+  appId: "1:730006798062:web:a6d6a056edb0b20db5bdff"
+};
+
+export default firebaseConfig;
